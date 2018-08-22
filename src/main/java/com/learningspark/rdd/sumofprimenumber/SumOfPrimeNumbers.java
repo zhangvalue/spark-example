6b9c779717153ad9bb8ad8188c6e6a48e0dfa0de
xiehui <615810790@qq.com>
2018-08-22 18:57:23 +0800

    Add sum of prime numbers in local file

---
+++ src/main/java/com/learningspark/rdd/sumofprimenumber/SumOfPrimeNumbers.java
@@ -0,0 +1,31 @@
+package com.learningspark.rdd.sumofprimenumber;
+
+import java.util.Arrays;
+
+import org.apache.hadoop.yarn.webapp.hamlet.HamletSpec._;
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+
+public class SumOfPrimeNumbers {
+
+	public static void main(String[] args) {
+		Logger.getLogger("org").setLevel(Level.ERROR);
+		
+		SparkConf conf = new SparkConf().setAppName("sumofprimenumber").setMaster("local[*]");
+		JavaSparkContext sc = new JavaSparkContext(conf);
+		
+		JavaRDD<String> lines = sc.textFile("in/prime_nums.text");
+		JavaRDD<String> numbers = lines.flatMap(line -> Arrays.asList(line.split("\\s+")).iterator());
+		
+		JavaRDD<String> validNumbers = numbers.filter(number -> !number.isEmpty());
+		JavaRDD<Integer> intNumbers = validNumbers.map(number -> Integer.valueOf(number));
+		
+		System.out.println("Sum is: " + intNumbers.reduce((x, y) -> x + y));
+		
+		sc.close();
+	}
+
+}
