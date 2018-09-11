961512d35a6c8eae84afd2fee2f01a1a33f4789a
xiehui <615810790@qq.com>
2018-09-11 10:28:53 +0800

    airports not in USA pair RDD

---
+++ src/main/java/com/learningspark/pairRdd/filter/AirportsNotInUsaProblem.java
@@ -0,0 +1,23 @@
+package com.learningspark.pairRdd.filter;
+
+public class AirportsNotInUsaProblem {
+
+	public static void main(String[] args) {
+		 
+		/* Create a Spark program to read the airport data from in/airports.text;
+		   generate a pair RDD with airport name being the key and country name being the value.
+		   Then remove all the airports which are located in United States and output the pair RDD to out/airports_not_in_usa_pair_rdd.text.
+		   
+		   Each row of the input file contains the following columns:
+		   Airport ID, Name of airport, Main city served by airport, Country where airport is located,
+		   IATA/FAA code, ICAO Code, Latitude, Longitude, Altitude, Timezone, DST, Timezone in 01son format.
+		 
+		   Sample output:
+		   
+           ("Kamloops", "Canada")
+           ("Wewak Intl", "Papua New Guinea")
+           ...
+		 */
+	}
+
+}
